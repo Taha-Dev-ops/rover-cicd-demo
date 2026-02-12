2e473847a7c01a5d287637e3c8cc2890329d79bf
Taha-Dev-ops <taha@example.com>
2026-02-12 21:42:45 +0000

    Added HelloRover Java application

---
+++ HelloRover.java
@@ -0,0 +1,7 @@
+public class HelloRover {
+    public static void main(String[] args) {
+        System.out.println("Hello from Rover CI/CD!");
+        System.out.println("Machine: Taha-Dev-ops testing server");
+        System.out.println("Date: " + new java.util.Date());
+    }
+}
